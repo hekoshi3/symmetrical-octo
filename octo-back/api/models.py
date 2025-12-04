e84fdabbd21b3205f3c78a40c8b3e74ce4f3b7bb
Kortis112 <redmolnia@gmail.com>
2025-12-05 03:50:47 +0700

    chore: changed models

--- octo-back/api/models.py
+++ octo-back/api/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 
 class CustomUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True)
-    avatar = models.ImageField(upload_to="users/avatars/", blank=True, default="static/default_avatar.jpeg", max_length=500) #TODO: Обработка NULL
+    avatar = models.ImageField(upload_to="users/avatars/", blank=True, null=True, max_length=500) #TODO: Обработка NULL
 
     def __str__(self):
         return self.user.username
@@ -16,35 +17,49 @@ class Post(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ForeignKey('GeneratedImage', null=True, on_delete=models.CASCADE)
+    aimodel = models.ForeignKey('AiModel', null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+    
+    def clean(self):
+        if not self.image and not self.aimodel:
+            raise ValidationError("Пост должен ссылаться на изображение или модель.")
+        if self.image and self.aimodel:
+            raise ValidationError("Пост должен ссылаться только на один объект.")
 
 class AiModel(models.Model):
     name = models.CharField(max_length=50)
-    AI_MODEL_TYPES = {
-        "LORA" : "LoRA",
-        "CHECKPOINT": "Checkpoint",
-    }
+    AI_MODEL_TYPES = [
+        ("LORA", "LoRA"),
+        ("CHECKPOINT", "Checkpoint"),
+        ("EMBEDDING", "Embedding"),
+        ("UPSCALER", "Upscaler"),
+        ("CONTROLNET", "ControlNet"),
+    ]
     model_type = models.CharField(max_length=50, choices=AI_MODEL_TYPES)
     file = models.FileField(upload_to="models/", max_length=500)
-    preview_image = models.ForeignKey('GeneratedImage', blank=True, related_name='model_featured', null=True, on_delete=models.SET_NULL)
-    description = models.TextField()
+    preview_image = models.ForeignKey('Post', blank=True, related_name='model_featured', null=True, on_delete=models.SET_NULL)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
 
+
+
 class GeneratedImage(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='images')
     image = models.ImageField(upload_to="images/", max_length=500)
-    model = models.ForeignKey(AiModel, on_delete=models.SET_NULL, null=True, blank=True)
-    prompt = models.TextField()
-    negative_prompt = models.TextField(blank=True)
-    seed = models.BigIntegerField()
-    steps = models.IntegerField()
-    cfg_scale = models.FloatField()
-    sampler = models.CharField(max_length=50)
+    checkpoint = models.ForeignKey(AiModel, on_delete=models.SET_NULL, limit_choices_to={'model_type': 'CHECKPOINT'}, related_name='model', null=True, blank=True)
+    resources = models.ManyToManyField(AiModel, related_name='additional_models', limit_choices_to={'model_type__in': ['LORA', 'EMBEDDING', 'UPSCALER', 'CONTROLNET']}, blank=True)
+    generation_params = models.JSONField(default=dict)
+    #prompt = models.TextField(blank=True)
+    #negative_prompt = models.TextField(blank=True)
+    #seed = models.BigIntegerField(null=True)
+    #steps = models.IntegerField(null=True)
+    #cfg_scale = models.FloatField(null=True)
+    #sampler = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
-        return self.prompt
+        return f"Image #{self.id}"
 
